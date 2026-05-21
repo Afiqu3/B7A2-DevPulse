14716123e56c441d4770f2a39f80393e32fba897
Afiqu3 <gahossainj@gmail.com>
2026-05-21 22:53:12 +0600

    Add login functionality with JWT authentication and error handling

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,15 +3,53 @@ import { authService } from "./auth.service";
 import sendResponse from "../../utility/sendResponse";
 
 const signUpUser = async (req: Request, res: Response) => {
-  const result = await authService.signUpUserIntoDB(req.body);
-  sendResponse(res, {
-    statusCode: 201,
-    success: true,
-    message: "User Created successfully!",
-    data: result.rows[0],
-  });
+  try {
+    const result = await authService.signUpUserIntoDB(req.body);
+    sendResponse(res, {
+      statusCode: 201,
+      success: true,
+      message: "User registered successfully",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    if (error.code === "23505") {
+      return sendResponse(res, {
+        statusCode: 409,
+        success: false,
+        message: error.message,
+        error: error,
+      });
+    }
+    sendResponse(res, {
+      statusCode: 500,
+      success: false,
+      message: error.message,
+      error: error,
+    });
+  }
+};
+
+const loginUser = async (req: Request, res: Response) => {
+  try {
+    const result = await authService.loginUserIntoDB(req.body);
+
+    sendResponse(res, {
+      statusCode: 200,
+      success: true,
+      message: "Login successful",
+      data: result,
+    });
+  } catch (error: any) {
+    sendResponse(res, {
+      statusCode: 401,
+      success: false,
+      message: error.message,
+      error: error,
+    });
+  }
 };
 
 export const authController = {
   signUpUser,
+  loginUser,
 };
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { pool } from "../../db";
 import type { IUser } from "./auth.interface";
+import config from "../../config";
 
 const signUpUserIntoDB = async (payload: IUser) => {
   const { name, email, password, role } = payload;
@@ -19,6 +21,53 @@ const signUpUserIntoDB = async (payload: IUser) => {
   return result;
 };
 
+const loginUserIntoDB = async (payload: {
+  email: string;
+  password: string;
+}) => {
+  const { email, password } = payload;
+  const userData = await pool.query(
+    `
+    SELECT * FROM users WHERE email=$1
+    `,
+    [email],
+  );
+  if (userData.rows.length === 0) {
+    throw new Error("Invalid Credentials!");
+  }
+
+  const user = userData.rows[0];
+  const matchPassword = await bcrypt.compare(password, user.password);
+
+  if (!matchPassword) {
+    throw new Error("Invalid Credentials!");
+  }
+
+  const jwtPayload = {
+    id: user.id,
+    name: user.name,
+    role: user.role,
+    email: user.email,
+  };
+
+  const accessToken = jwt.sign(jwtPayload, config.secret, {
+    expiresIn: "1d",
+  });
+
+  return {
+    token: accessToken,
+    user: {
+      id: user.id,
+      name: user.name,
+      role: user.role,
+      email: user.email,
+      created_at: user.created_at,
+      updated_at: user.updated_at
+    },
+  };
+};
+
 export const authService = {
   signUpUserIntoDB,
+  loginUserIntoDB,
 };
